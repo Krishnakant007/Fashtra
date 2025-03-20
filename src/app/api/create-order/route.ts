5f9352e464fdeaf05b8fff82e81b49f37481e910
Krishnakant chavan <70923432+Krishnakant007@users.noreply.github.com>
2025-03-20 16:44:24 +0530

    Update route.ts

--- src/app/api/create-order/route.ts
+++ src/app/api/create-order/route.ts
@@ -27,7 +27,6 @@
 // }
 
 
-
 import { NextRequest, NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
@@ -38,20 +37,20 @@ const razorpay = new Razorpay({
 
 export async function POST(request: NextRequest) {
   try {
-    const { amount } = await request.json();
+    const { amount }: { amount: number } = await request.json();
     if (!amount) throw new Error("Amount not provided");
 
     const order = await razorpay.orders.create({
-      amount: amount * 1,
+      amount: amount * 100, // Razorpay expects amount in paise (100 paise = 1 INR)
       currency: "INR",
       receipt: `receipt_${Date.now()}`,
     });
 
     return NextResponse.json({ orderId: order.id }, { status: 200 });
-  } catch (error: any) {
+  } catch (error) {
     console.error("Error creating order:", error);
     return NextResponse.json(
-      { error: error.message || "Error creating order" },
+      { error: (error as Error).message || "Error creating order" },
       { status: 500 }
     );
   }
